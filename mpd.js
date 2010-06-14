f32adc9fe1e6aab9a1b2d1e5e2ec55615b3c37a6
Orlando Vazquez <ovazquez@gmail.com>
2010-06-14 01:39:08 -0700

    wip

--- mpd.js
+++ mpd.js
@@ -37,7 +37,7 @@ events = require('events');
 puts = sys.puts;
 inspect = sys.inspect;
 
-function MPD(port, host) {
+exports.MPD = MPD = function (port, host) {
   var self = this;
 
   this.port = port || 6600;
@@ -46,19 +46,30 @@ function MPD(port, host) {
   this.data = '';
   this.linesBuffer = [];
   this.carry = '';
-  this.cmdQueue = [];
 
-  this.cmdQueue.push(function (error, result) {
-    if (!result[0].match(/^OK MPD \d+\.\d+\.\d+/)) {
-      self.emit("error", "Got invalid hello msg from mpd " + result);
-    }
-  });
+  this.initializeCommands();
 }
 
 sys.inherits(MPD, events.EventEmitter);
 
+var helloRE = new RegExp('^OK MPD\\s+(.*)');
 MPD.prototype.connect = function (callback) {
   var self = this;
+  this.cmdQueue = [];
+
+  // Handler for the hello message
+  this.cmdQueue.push(function (error, result) {
+          puts(result);
+    var m = helloRE.exec(result[0].trim());
+
+    if (!m) {
+      self.emit("error", "Got invalid hello msg from mpd " + result);
+    }
+    else {
+      puts("MPD Server running version: " + m[1]);
+    }
+  });
+
   this.conn = net.createConnection(this.port, this.host);
   this.conn.addListener('connect', function () {
     self.addListeners();
@@ -66,13 +77,136 @@ MPD.prototype.connect = function (callback) {
   });
 }
 
-MPD.prototype.close = function () {
+MPD.prototype.disconnect = function () {
   this.conn.end();    
 };
 
+var kvRE = RegExp('^([^:]+): (.*)');
+MPD.prototype.initializeCommands = function () {
+  var self = this;
+//   this._fetch_list = function () {};
+//   this._fetch_nothing = function () {};
+//   this._fetch_nothing = function () {};
+//   this._fetch_nothing = function () {};
+
+  this._fetch_nothing = function (lines, callback) {
+    callback(null);
+  };
+
+  this._fetch_object = function (lines, callback) {
+    var item = {};
+    var kv, m, line;
+    lines.pop();
+    for (i in lines) {
+      line = lines[i].trim();
+      m = kvRE.exec(line);
+      if (m) {
+        item[m[1]] = m[2];
+      }
+      else {
+        return callback(new Error("Invalid key/value string "
+                        + inspect(line)));
+      }
+    }
+    callback(null, item);
+  };
+
+  this._commands = {
+    // Status Commands
+    "clearerror":       this._fetch_nothing,
+    "currentsong":      this._fetch_object,
+    "idle":             this._fetch_list,
+    "noidle":           null,
+    "status":           this._fetch_object,
+    "stats":            this._fetch_object,
+    // Playback Option Commands
+    "consume":          this._fetch_nothing,
+    "crossfade":        this._fetch_nothing,
+    "random":           this._fetch_nothing,
+    "repeat":           this._fetch_nothing,
+    "setvol":           this._fetch_nothing,
+    "single":           this._fetch_nothing,
+    "volume":           this._fetch_nothing,
+    // Playback Control Commands
+    "next":             this._fetch_nothing,
+    "pause":            this._fetch_nothing,
+    "play":             this._fetch_nothing,
+    "playid":           this._fetch_nothing,
+    "previous":         this._fetch_nothing,
+    "seek":             this._fetch_nothing,
+    "seekid":           this._fetch_nothing,
+    "stop":             this._fetch_nothing,
+    // Playlist Commands
+    "add":              this._fetch_nothing,
+    "addid":            this._fetch_item,
+    "clear":            this._fetch_nothing,
+    "delete":           this._fetch_nothing,
+    "deleteid":         this._fetch_nothing,
+    "move":             this._fetch_nothing,
+    "moveid":           this._fetch_nothing,
+    "playlist":         this._fetch_playlist,
+    "playlistfind":     this._fetch_songs,
+    "playlistid":       this._fetch_songs,
+    "playlistinfo":     this._fetch_songs,
+    "playlistsearch":   this._fetch_songs,
+    "plchanges":        this._fetch_songs,
+    "plchangesposid":   this._fetch_changes,
+    "shuffle":          this._fetch_nothing,
+    "swap":             this._fetch_nothing,
+    "swapid":           this._fetch_nothing,
+    // Stored Playlist Commands
+    "listplaylist":     this._fetch_list,
+    "listplaylistinfo": this._fetch_songs,
+    "listplaylists":    this._fetch_playlists,
+    "load":             this._fetch_nothing,
+    "playlistadd":      this._fetch_nothing,
+    "playlistclear":    this._fetch_nothing,
+    "playlistdelete":   this._fetch_nothing,
+    "playlistmove":     this._fetch_nothing,
+    "rename":           this._fetch_nothing,
+    "rm":               this._fetch_nothing,
+    "save":             this._fetch_nothing,
+    // Database Commands
+    "count":            this._fetch_object,
+    "find":             this._fetch_songs,
+    "list":             this._fetch_list,
+    "listall":          this._fetch_database,
+    "listallinfo":      this._fetch_database,
+    "lsinfo":           this._fetch_database,
+    "search":           this._fetch_songs,
+    "update":           this._fetch_item,
+    // Connection Commands
+    "close":            null,
+    "kill":             null,
+    "password":         this._fetch_nothing,
+    "ping":             this._fetch_nothing,
+    // Audio Output Commands
+    "disableoutput":    this._fetch_nothing,
+    "enableoutput":     this._fetch_nothing,
+    "outputs":          this._fetch_outputs,
+    // Reflection Commands
+    "commands":         this._fetch_list,
+    "notcommands":      this._fetch_list,
+    "tagtypes":         this._fetch_list,
+    "urlhandlers":      this._fetch_list,
+  };
+
+  var keys = Object.keys(this._commands);
+  var command;
+
+  for (commandNum in keys) {
+    puts("Setting handler for " + keys[commandNum]);
+    this[keys[commandNum]] = function (args, callback) {
+      puts("About to run " + keys[commandNum]);
+      self.cmd(keys[commandNum], args, callback);
+    }
+  }
+}
+
+
 var okRE = new RegExp('^OK\s?');
 
-MPD.prototype.addListeners = function (connCallback) {
+MPD.prototype.addListeners = function () {
   var self = this;
 
   this.conn.addListener('error', function (exception) {
@@ -126,11 +260,19 @@ MPD.prototype.addListeners = function (connCallback) {
 // mpd.cmd('command', [ arg0, arg1 ], callback)
 // mpd.cmd('command', callback)
 MPD.prototype.cmd = function (command, args, callback) {
+  var self = this;
   if (!callback) {
     callback = args;
   }
-  puts("Running command");
-  this.cmdQueue.push(callback);
+  puts("Running command " + command);
+  this.cmdQueue.push(function(error, result) {
+    if (error) return callback(error);
+
+    puts("found command, " + command + " " + inspect(self._commands[command]));
+    var fun = self._commands[command];
+    fun(result, callback);
+  });
+
   this.conn.write(command + "\n");
 }
 
@@ -146,10 +288,15 @@ mpd.connect(function (error, result) {
     throw error;
   }
 
-  mpd.cmd('currentsong', [], function (error, result) {
+//   mpd.cmd('currentsong', [], function (error, result) {
+     mpd.count([], function (error, result) {
     if (error) throw error;
-    puts("Got result for playlistinfo " + inspect(result));
-    mpd.close();
+    puts(inspect(result));
+//     mpd.currentsong([], function (error, result) {
+//       if (error) throw error;
+//       puts(inspect(result));
+      mpd.disconnect();
+//     });
   });
 });
 
